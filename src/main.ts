1b1a5a2343dcb178a06dbd842137efd6eaa66a2f
YASSERRMD <arafath.yasser@gmail.com>
2026-07-08 05:28:47 +0400

    build(vite): scaffold vite + typescript project

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+app.innerHTML = `
+  <h1>Recallwell Nano</h1>
+  <p>On-device, vectorless knowledge-base Q&A</p>
+`
